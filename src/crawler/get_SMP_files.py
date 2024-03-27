2a2b807ea15f7c00ec486b02bc7c7ec337f49144
ThanosPourikis <thanos.pourikis@gmail.com>
2024-03-27 17:51:01 +0200

    refactoring for new architecture

--- src/crawler/get_SMP_files.py
+++ src/crawler/get_SMP_files.py
@@ -1,41 +1,32 @@
 import os
-from posix import listdir
-import requests
 import re
-from zipfile import ZipFile
+from datetime import datetime
+from os import listdir
+from pathlib import Path
 
-export = []
-folder_path = "smp_files/"
+import requests
 
 
-def get_SMP_files():
+def get_smp_files(last_smp_date: datetime, folder_path: Path):
     if os.path.exists(folder_path):
         print("Directory already exists")
     else:
         print("Creating Directory")
         os.mkdir(folder_path)
-
-    # zip file of previous year from 2020-11-01
-    url = "https://www.enexgroup.gr/el/c/document_library/get_file?uuid=23d04ca0-5850-415b-4c7b-d0a65ed293cf&groupId=20126"
-
-    zip_path = folder_path + "2020_EL-DAM_ResultsSummary.zip"
-    if os.path.exists(zip_path):
-        print("Processing Zip file")
-        with ZipFile(zip_path, "r") as zip_file:
-            zip_file.extractall(folder_path)
-    else:
-        print("Downloading Zip file")
-        with open(zip_path, "wb") as zip_file:
-            zip_file.write(requests.get(url).content)
-        print("Processing Zip file")
-
-        with ZipFile(zip_path, "r") as zip_file:
-            zip_file.extractall(folder_path)
-
+    # Search Archive for files if year != current year
+    if datetime.now().year != last_smp_date.year:
+        pass
     flag = False
     index = 1
     while True:
-        url = f"https://www.enexgroup.gr/el/web/guest/markets-publications-el-day-ahead-market?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2_delta=7&p_r_p_resetCur=false&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2_cur={index}"
+        url = (
+            f"https://www.enexgroup.gr/el/web/guest/markets-publications-el-day-ahead-market?p_p_id"
+            f"=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2&p_p_lifecycle=0"
+            f"&p_p_state=normal&p_p_mode=view"
+            f"&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2_delta=7"
+            f"&p_r_p_resetCur=false"
+            f"&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_9CZslwWTpeD2_cur={index}"
+        )
 
         data = requests.get(url)
         print(data)
